e4468ce3a33d84434f208c89c3b35f2a5ea31046
Cole <Logan.Cole@cdk.com>
2022-01-17 11:30:46 -0700

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Clint side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = 'Error'
+                messageTwo.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
